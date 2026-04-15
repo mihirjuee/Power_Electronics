2bb2c1e011d65fac5f409476696a5e5b274e8c91
mihirjuee <mihir.juee@gmail.com>
2026-04-15 10:09:45 +0530

    Update Half_Wave_Rectifier.py

--- Half_Wave_Rectifier.py
+++ Half_Wave_Rectifier.py
@@ -44,7 +44,7 @@ def draw_circuit(R, Vm, use_filter=False, C_uF=0):
         d.add(elm.Line().right(2.5))   # shift right
         d.add(elm.Line().down())
         d.add(elm.Capacitor().label(f'C\n{C_uF}µF'))
-        d.add(elm.Line().left().length(4.5))  # connect to return path
+        d.add(elm.Line().left().length(5.5))  # connect to return path
         d.add(elm.Line().up())
         d.pop()
 
