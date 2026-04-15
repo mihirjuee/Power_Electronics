61781930dd7c1747731675944e904f1418d7a133
mihirjuee <mihir.juee@gmail.com>
2026-04-15 09:48:51 +0530

    Update Half_Wave_Rectifier.py

--- Half_Wave_Rectifier.py
+++ Half_Wave_Rectifier.py
@@ -109,7 +109,12 @@ with tab1:
     with col1:
         st.markdown("### Circuit")
         mid_index = len(diode_on_array)//2
-        st.image(draw_circuit(R, Vm, diode_on_array[mid_index]))
+        st.image(draw_circuit(
+    R, Vm,
+    diode_on_array[mid_index],
+    use_filter,
+    C_uF
+))
 
         if diode_on_array[mid_index]:
             st.success("Diode ON")
