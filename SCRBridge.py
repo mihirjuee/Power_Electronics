e7a303ceb4ae3c7c9f8eb996cfe1733f9ae38ff5
mihirjuee <mihir.juee@gmail.com>
2026-04-26 01:48:04 +0530

    Update SCRBridge.py

--- SCRBridge.py
+++ SCRBridge.py
@@ -57,20 +57,17 @@ def draw_circuit(pair):
         d += elm.Line().right()
         d += elm.Line().down()
         d += (L1 := elm.Resistor().down().label("Load"))
-        d += elm.Line().down(1)
+        d += elm.Line().down(2)
         d += elm.Line().left(1)
+        d.push()
         d += elm.Line().up()
         d += (T2 := elm.SCR().label("T2").color(c1))
         d += (T3 := elm.SCR().label("T3").color(c2))
         d.pop()
         d += elm.Line().down(1)
         d += (T4 := elm.SCR().label("T4").color(c2).reverse())
-                
-        # Return to source node for bottom diodes
-        d.push()
-        
         d.pop()
-        
+        d += elm.Line().left(1)
         return d.fig
 
 # Render using the figure object directly
