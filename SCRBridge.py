d38679170b0d7a5c0ed92176623a5b4cd426c14c
mihirjuee <mihir.juee@gmail.com>
2026-04-26 12:23:46 +0530

    Update SCRBridge.py

--- SCRBridge.py
+++ SCRBridge.py
@@ -75,9 +75,9 @@ def draw_circuit(pair):
         d += elm.Line().down(1)
         d += elm.Line().left(1)
         d.pop()
-        d += elm.Line().down(1.75)
+        d += elm.Line().down(1.85)
         d += (T4 := elm.SCR().label("T4").color(c2).reverse())
-        d += elm.Line().down(0.25)
+        d += elm.Line().down(0.15)
         d += elm.Line().right(2)
         return d.fig
 
