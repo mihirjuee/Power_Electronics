91ac2856fbaa71603098f3cc33133dff4bccb5d5
mihirjuee <mihir.juee@gmail.com>
2026-04-26 12:35:14 +0530

    Update SCRBridge.py

--- SCRBridge.py
+++ SCRBridge.py
@@ -60,7 +60,7 @@ def draw_circuit(pair):
         d += elm.Line().right()
         d += elm.Line().down()
         d += (L1 := elm.Resistor().down().label("Load"))
-        d += elm.Line().down(1.6)
+        d += elm.Line().down(1.5)
         d += elm.Line().left(1)
        
         d += elm.Line().up(0.05)
