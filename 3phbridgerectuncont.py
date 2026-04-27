718711eb755f884d660baaa322f728d23ecd7808
mihirjuee <mihir.juee@gmail.com>
2026-04-27 19:23:42 +0530

    Update 3phbridgerectuncont.py

--- 3phbridgerectuncont.py
+++ 3phbridgerectuncont.py
@@ -52,8 +52,9 @@ with schemdraw.Drawing() as d:
     d += elm.Line().right(2)
 
     # ================= TOP DIODES =================
-    d += elm.Line().at(S1.end).up(5)
+    d += elm.Line().at(S1.end).up(4.5)
     D1 = d.add(elm.Diode().up(2).label("D1"))
+    d += elm.Line().up(0.5)
     d += elm.Line().at(S2.end).right(2)
     d.push()
     d += elm.Line().up(2)
